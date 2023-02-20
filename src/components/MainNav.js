d9d66bb6edd65b1af29cd513029b0e2b65119e2c
LeeJina <97230811+cocojina@users.noreply.github.com>
2023-02-20 21:14:55 +0900

    MainNav first commit

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,28 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import { Outlet } from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+import { BsArrowRightCircle } from "react-icons/bs";
 const CategorisBlock = styled.div`
   display: flex;
   padding: 1rem;
 `;
-const Category = styled.div`
+const Category = styled(Link)`
+  display: flex;
   cursor: pointer;
   padding-right: 1rem;
-
+  color: black;
+  text-decoration: none;
   &:hover {
     color: #4f85eb;
   }
+`;
+const RightArrow = styled(BsArrowRightCircle)`
+  padding-top: 3px;
 `;
 const MainNav = () => {
   return (
     <>
       <CategorisBlock>
-        <Category>KANGNAM LIKELION</Category>
-        <Category>소개</Category>
-        <Category>트랙</Category>
-        <Category>홍보</Category>
-        <Category>어플라이</Category>
+        <Category to="/">LIKELION KNU</Category>
+        <Category to="/intro">소개</Category>
+        <Category to="/track">트랙</Category>
+        <Category to="promote">홍보</Category>
+        <Category to="https://knu-likelion.org/" target="_blank">
+          어플라이
+          <div>
+            <RightArrow />
+          </div>
+        </Category>
       </CategorisBlock>
       <Outlet />
     </>
